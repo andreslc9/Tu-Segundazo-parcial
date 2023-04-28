c820def3c1ad09f943c8893aae8e54c0717f34f5
andreslombo <a.lombo@uniandes.edu.co>
2023-04-27 19:29:19 -0500

    Se agrega clase vehiculo y servicio de consumo de api

---
+++ src/app/vehicle/vehicle.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'enviroments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+import { Vehicle } from './vehicle';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehicleService {
+  private apiUrl: string = environment.baseUrl;
+  constructor(private http: HttpClient) { }
+
+  getVehicles(): Observable<Vehicle[]> {
+    return this.http.get<Vehicle[]>(this.apiUrl);
+  }
+
+
+}
