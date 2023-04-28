4e7d58308963cfabd55002e5ec8d1db6941bbae5
andreslombo <a.lombo@uniandes.edu.co>
2023-04-27 19:38:32 -0500

    El componente lista de vehiculos ahora consume el servicio

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { VehicleService } from '../vehicle.service';
+import { Vehicle } from '../vehicle';
 
 @Component({
   selector: 'app-vehicle-list',
+  providers: [VehicleService],
   templateUrl: './vehicle-list.component.html',
   styleUrls: ['./vehicle-list.component.css']
 })
 export class VehicleListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private vehicleService: VehicleService) { }
+
+  vehicles: Array<Vehicle>;
+
+  getVehicles() {
+    this.vehicleService.getVehicles().subscribe(vehicles => {
+      this.vehicles = vehicles;
+      console.log(this.vehicles);
+    });
+  }
 
   ngOnInit() {
+    this.getVehicles();
   }
 
 }
