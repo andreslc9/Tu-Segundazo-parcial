c63239266c970c625c1672e638df74b9e694b5f4
andreslombo <a.lombo@uniandes.edu.co>
2023-04-27 19:33:30 -0500

    Se agrega componente de lista de vehículos

--- src/app/vehicle/vehicle.module.ts
+++ src/app/vehicle/vehicle.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VehicleComponent } from './vehicle.component';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [VehicleComponent]
+  declarations: [VehicleListComponent],
+  exports: [VehicleListComponent]
 })
 export class VehicleModule { }
